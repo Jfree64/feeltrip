1918516a4c3b20dc7450ed031b758167aa59230a
Jono Freeman <jfree64@gmail.com>
2018-05-20 21:18:47 -0400

    Combined App.js into index.js
    Also added a bunch of node modules (I have no idea what I'm doing pls help)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import './index.css';
-import App from './App';
+import {navbar} from './navbar.jsx';
 import registerServiceWorker from './registerServiceWorker';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <navbar />
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to feeltrip</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
