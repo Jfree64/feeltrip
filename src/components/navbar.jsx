1fafde9f676cdea00764f38ac5f4f7b8d0a9d186
Jono Freeman <jfree64@gmail.com>
2018-05-29 21:04:49 -0400

    Some Test Stuff I Guess
    God why did I try to code my own website?

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,9 +6,15 @@ class NavBar extends Component {
             <header>
                 <ul id="headerButtons">
                     <li className="navButton"><Link to="">Home</Link></li>
+<<<<<<< Updated upstream:src/navbar.jsx
                     <li className="navButton"><Link to="/about">About</Link></li>
                     <li className="navButton"><Link to="/about">Home</Link></li>
                     <li className="navButton"><Link to="/about">Home</Link></li>
+=======
+                    <li className="navButton"><Link to="">About</Link></li>
+                    <li className="navButton"><Link to="">Map</Link></li>
+                    <li className="navButton"><Link to="">Blog</Link></li>
+>>>>>>> Stashed changes:src/components/navbar.jsx
                 </ul>
             </header>
         )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
+=======
+
+import { Route, Link, MemoryRouter } from 'react-router-dom'
+
+>>>>>>> Stashed changes
 import logo from './logo.svg';
 import {BrowserRouter} from 'react-router-dom'
 import './App.css';
 import './index.css';
+<<<<<<< Updated upstream
 import NavBar from './NavBar.jsx';
+=======
+import NavBar from './components/navbar.jsx';
+>>>>>>> Stashed changes
 import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
@@ -14,7 +24,7 @@ class App extends Component {
         <NavBar />
           <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to feeltrip</h1>
+          <h1 className="App-title">Welcome to felttrip</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
