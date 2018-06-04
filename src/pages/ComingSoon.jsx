a950036aecfed4d58e21a51bcb34dced18810784
Jono Freeman <jfree64@gmail.com>
2018-06-04 09:22:41 -0400

    COMING SOON PAGE
    Fonts don't work on google, tho...

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 class Donate extends Component {
     render() {
         return (
-          <div>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          Coming Soon!
-          </div>
+          <section class="hero is-large">
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              <h1 class="title">
+                COMING SOON
+              </h1>
+              <h2 class="subtitle">
+                building a website is hard, guys...
+              </h2>
+            </div>
+            </div>
+          </section>
         )
     }
 }
