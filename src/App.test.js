ce06ca28aedb0e60c203c744e63c50563c620a17
Jono Freeman <jfree64@gmail.com>
2018-06-07 10:32:23 -0400

    Trying to install Enzyme.js and fix testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+{/*import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index.js';
 
@@ -6,4 +6,15 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+}); */}
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './index.js';
+
+const wrapper = shallow(<MyComponent/>);
+
+describe('(Component) App', () => {
+  it('renders without exploding', () => {
+    expect(wrapper).toHaveLength(1)
+  });
 });
