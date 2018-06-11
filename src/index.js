a1c3ee3a6ef488a6402976ebb65879f038a166d2
Jono Freeman <jfree64@gmail.com>
2018-06-11 11:24:44 -0400

    Fixing webpack config for the nth time...
    Never remembers to build SCSS... Why?

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-120672166-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render((
 <BrowserRouter>
