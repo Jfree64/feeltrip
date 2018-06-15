5297f72cfb553b4781151ecec0527346ec2110a0
Jono Freeman <jfree64@gmail.com>
2018-06-15 16:48:11 -0400

    There we go (accidental "q" in the scss)

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageHeader = (props) => (
   <section className={`hero is-bold`}>
     <img src={props.img} className="page-header" />
-    <div className="hero-body page-header-title">
+    <div className="page-header-title">
       <div className="container">
         <h1 className="title">
           {props.title}
