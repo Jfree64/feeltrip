df1df977563b3791e1f7a6fbc115cacc1dc85086
Jono Freeman <jfree64@gmail.com>
2018-08-08 17:56:04 -0400

    Updated info in "About me" section

--- src/pages/about/FAQ.jsx
+++ src/pages/about/FAQ.jsx
@@ -8,18 +8,18 @@ const FAQ = () => (
       <div className="column text-is-centered">
       <h3 className='question'>Can I join you?</h3>
 <p>We welcome anyone who would like to join us for any length of the route.
-Please note that this ride is entirely unsupported and, although planned, subject to daily changes based on weather restrictions</p>
+Please note that this ride is entirely unsupported and, although we have a route planned, subject to daily changes based on weather, road closures, and other random acts of adventure.</p>
 <br/>
 <h3 className='question'> Can I help you raise money? </h3>
 <p>You can absolutely. Please get in touch and let us know how you'd like to help.</p>
 <br/>
 <h3 className='question'> Where is the money going?</h3>
-<p>We’re donating 100% of the net proceeds of the ride to NAMI as a DIY project. Depending on how much money we raise, we’ll be covering an appropriate portion of the ride expenses. You can read a detailed breakdown of the donation plan here (forthcoming)</p>
+<p>We’re donating 100% of the funds raised to NAMI as a DIY project. You can read a detailed breakdown of how NAMI uses the money we donate on their website (link forthcoming).</p>
 <br/>
 <h3 className='question'> Is my donation tax deductable?</h3>
-<p>If you donate via our NAMI page, your donation is 100% tax deductable. Purchases of goods, cash/venmo donations, etc. are generally not deductable unless you reach out to us.</p>
+<p>If you donate via this website or our NAMI page, your donation is 100% tax deductable. Purchases of goods, cash/venmo donations, etc. are generally not deductable unless you reach out to us.</p>
 <br/>
-<h3 className='question'> What will they do with the money?</h3>
+<h3 className='question'> What will NAMI do with the money you raise?</h3>
 <p>You can read more about the services offered by NAMI here (link).</p>
       </div>
   </div>
